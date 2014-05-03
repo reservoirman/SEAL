b00a6e01c4bc29d4ad8f60ea81df4e12b988ddec
Ten-Seng Guh <tg2458@columbia.edu>
2014-05-02 22:07:40 -0700

    added SEAL_Util to library; implemented the swap and move (Address = ) functions.  String function remaining.

---
+++ SEAL_Util.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "SEAL_Util.h"
+
+void * SEALUtil_Swap(void *object, int length)
+{
+	int i;
+	char temp;
+	char *array = (char *)object;
+	int arraymax = length - 1;
+	for (i = 0; i < (length / 2); i++)
+	{
+		temp = array[i];
+		array[i] = array[arraymax - i];
+		array[arraymax - i] = temp;
+	}
+
+	return object;
+}
+
+char * SEALUtil_String(void *object, int length)
+{
+
+}
+
+void * SEALUtil_Move(void * object, void *location, int length)
+{
+	return memmove(location, object, (size_t)length);
+}
+#define SEALUtil_Length(x) sizeof(x)
---
+++ SEAL_Util.h
@@ -0,0 +1,11 @@
+#ifndef __SEAL_UTIL_H__
+#define __SEAL_UTIL_H__
+
+
+void * SEALUtil_Swap(void *object, int length);
+
+char * SEALUtil_String(void *object, int length);
+
+int SEALUtil_Length(void *object);
+
+#endif
